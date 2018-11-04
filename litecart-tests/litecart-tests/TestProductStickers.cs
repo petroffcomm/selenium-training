4391a53244dd05a4591740c9f31488a07d2812ed
petroffcomm <petroffcomm@gmail.com>
2018-11-04 02:39:16 +0200

    * chromedriver: * TestProductStickers.cs:
    * geckodriver: ДЗ8.
    Сценарий, проверяющий наличие стикеров у всех товаров в учебном
      приложении litecart на главной странице.
    Сценарий должен проверять, что у каждого товара имеется ровно один
      стикер.

---
+++ litecart-tests/litecart-tests/TestProductStickers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace LitecartTests
+{
+    [TestFixture]
+    public class ProductStickerTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("start-fullscreen");
+            chromeOptions.AddArgument("disable-infobars");
+            driver = new ChromeDriver(chromeOptions);
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(7));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+            driver.Url = "http://localhost/litecart";
+        }
+
+        [Test]
+        public void TestProductHasSingleSticker()
+        {
+            List<IWebElement> products = new List<IWebElement>(
+                driver.FindElements(
+                    By.CssSelector("li[class *= product]")
+                )
+            );
+
+            foreach(IWebElement product in products)
+            {
+                int stickersCount = product.FindElements(
+                    By.CssSelector("div[class *= sticker]")).Count;
+                string title = product.FindElement(By.CssSelector("a.link")).GetAttribute("title");
+
+                Assert.AreEqual(stickersCount, 1, "[{0}] product has {1} stickers", title, stickersCount);
+            }
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
