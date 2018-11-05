8cca49953831afccd21cf384cb0afbee576992bc
petroffcomm <petroffcomm@gmail.com>
2018-11-05 12:18:06 +0200

    structural changes

--- litecart-tests/litecart-tests/TestLogin.cs
+++ litecart-tests/litecart-tests/TestLogin.cs
@@ -2,6 +2,8 @@ using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
 using OpenQA.Selenium.Support.UI;
 
 namespace LitecartTests
@@ -15,13 +17,34 @@ namespace LitecartTests
         [SetUp]
         public void SetUp()
         {
-            FirefoxOptions firefoxOptions = new FirefoxOptions();
-            firefoxOptions.BrowserExecutableLocation = "/Applications/Firefox 2.app/Contents/MacOS/firefox";
-            firefoxOptions.UseLegacyImplementation = true;
+            string browser = "ff";
+            switch (browser)
+            {
+                case "ff":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    //firefoxOptions.BrowserExecutableLocation = "/Applications/Firefox 2.app/Contents/MacOS/firefox";
+                    firefoxOptions.BrowserExecutableLocation = "/Applications/Firefox Nightly.app/Contents/MacOS/firefox";
+                    firefoxOptions.UseLegacyImplementation = false;
 
-            driver = new FirefoxDriver(firefoxOptions);
+                    driver = new FirefoxDriver(firefoxOptions);
+                    //driver = new FirefoxDriver();
+                    break;
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("start-fullscreen");
+                    chromeOptions.AddArgument("disable-infobars");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+                case "safari":
+                    driver = new SafariDriver();
+                    break;
+            }
+
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(7));
             wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+            // Show browser capabilities
+            Console.Out.WriteLine( ((IHasCapabilities)driver).Capabilities);
         }
 
         [Test]
@@ -39,6 +62,13 @@ namespace LitecartTests
         [TearDown]
         public void TearDown()
         {
+            // Show cookies
+            Console.Out.WriteLine("Cookies sent by site:");
+            foreach (Cookie cookie in driver.Manage().Cookies.AllCookies)
+            {
+                Console.Out.WriteLine("{0} = {1}", cookie.Name, cookie.Value);
+            }
+
             driver.Quit();
         }
     }
